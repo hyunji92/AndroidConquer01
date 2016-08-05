240a09ddf845410f0faf3ce11f9f57337616b77a
hyunji92 <chunghj09@gmail.com>
2016-08-05 17:59:04 +0900

    canvas / paint / bitmap 예제진행중

--- app/src/main/java/study07/androidconquer01/view/MyView.java
+++ app/src/main/java/study07/androidconquer01/view/MyView.java
@@ -1,6 +1,7 @@
 package study07.androidconquer01.view;
 
 import android.content.Context;
+import android.graphics.Bitmap;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
@@ -11,13 +12,34 @@ import android.view.View;
  */
 public class MyView extends View {
 
+    private  Paint mPnt;
+    
     public MyView(Context context) {
-        super(context); 
+        super(context);
+
+        mPnt.setAntiAlias(true);
+        mPnt.setColor(Color.BLUE);
+        mPnt.setStrokeWidth(10);
+        mPnt.setStyle(Paint.Style.STROKE);
+        mPnt.setStrokeJoin(Paint.Join.BEVEL);
     }
+
     public void onDraw(Canvas canvas){
-        canvas.drawColor(Color.GRAY);
-        Paint paint = new Paint();
-        paint.setColor(Color.DKGRAY);
-        canvas.drawCircle(100,100,80,paint);
+        
+        /*Paint paint = new Paint();
+        paint.setColor(Color.GRAY);
+        canvas.drawCircle(100,100,80,paint);*/
+        canvas.drawColor(Color.LTGRAY);
+        canvas.drawRect(10,10,300,100,mPnt);
+
+        Bitmap BackBit =  Bitmap.createBitmap(300, 200, Bitmap.Config.ARGB_8888);
+        Canvas offscreen = new Canvas(BackBit);
+        offscreen.drawColor(Color.DKGRAY);
+        mPnt.setColor(Color.GREEN);
+        for(int x = 0; x < 300; x+= 10){
+            offscreen.drawLine(x, 0, 300 - x, 300, mPnt);
+        }
+
+        canvas.drawBitmap(BackBit, 10, 10, null);
     }
 }
